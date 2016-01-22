51fc2b7ae89612713153d0aeaafddad3451c1750
Pip3r4o <pip3r4o@gmail.com>
2016-01-23 01:10:36 +0200

    Scaffolded initial project

---
+++ Source/GalaxyStorm/Web/GalaxyStorm.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GalaxyStorm.Web.MvcApplication" Language="C#" %>
---
+++ Source/GalaxyStorm/Web/GalaxyStorm.Web/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(GalaxyStorm.Web.Startup))]
+namespace GalaxyStorm.Web
+{
+    using Owin;
+
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
