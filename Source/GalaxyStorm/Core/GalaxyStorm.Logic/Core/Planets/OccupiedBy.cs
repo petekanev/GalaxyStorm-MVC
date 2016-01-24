bdef3267ee13111b5368b7daff9ef8f0365961d5
Pip3r4o <pip3r4o@gmail.com>
2016-01-24 10:32:02 +0200

    Added planet object Added shard and planet names

---
+++ Source/GalaxyStorm/Core/GalaxyStorm.Logic/Core/Planets/OccupiedBy.cs
@@ -0,0 +1,10 @@
+﻿namespace GalaxyStorm.Logic.Core.Planets
+{
+    using System;
+
+    public enum OccupiedBy
+    {
+        Neutral = 0,
+        Player = 1
+    }
+}
---
+++ Source/GalaxyStorm/Core/GalaxyStorm.Logic/Core/Planets/Planet.cs
@@ -0,0 +1,21 @@
+﻿namespace GalaxyStorm.Logic.Core.Planets
+{
+    using System;
+
+    public class Planet
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public string Name { get; set; }
+
+        public double EnergyModifier { get; set; }
+
+        public double CrystalModifier { get; set; }
+
+        public double MetalModifier { get; set; }
+
+        public OccupiedBy OccupiedBy { get; set;}
+    }
+}
