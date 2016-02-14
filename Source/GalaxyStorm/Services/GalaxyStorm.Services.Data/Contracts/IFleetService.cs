a5c3ba0387b9ee4c88a210fe917a3b6c07f7e0f8
Pip3r4o <pip3r4o@gmail.com>
2016-02-14 02:22:25 +0200

    Implemented FleetService for scheduling recruitments

--- Source/GalaxyStorm/Services/GalaxyStorm.Services.Data/Contracts/IFleetService.cs
+++ Source/GalaxyStorm/Services/GalaxyStorm.Services.Data/Contracts/IFleetService.cs
@@ -5,6 +5,20 @@
 
     public interface IFleetService
     {
+        TimeSpan? ScheduleRecruitScout(string userId, int amount);
+
+        TimeSpan? ScheduleRecruitCarrier(string userId, int amount);
+
+        TimeSpan? ScheduleRecruitFighter(string userId, int amount);
+
+        TimeSpan? ScheduleRecruitInterceptor(string userId, int amount);
+
+        TimeSpan? ScheduleRecruitBomber(string userId, int amount);
+
+        TimeSpan? ScheduleRecruitJuggernaut(string userId, int amount);
+
+        void CompleteRecruiting(string userId);
+
         Units GetPlayerFleet(string userId);
     }
 }
--- Source/GalaxyStorm/Services/GalaxyStorm.Services.Data/FleetService.cs
+++ Source/GalaxyStorm/Services/GalaxyStorm.Services.Data/FleetService.cs
@@ -8,6 +8,8 @@
     using GalaxyStorm.Data.Models.PlayerObjects;
     using GalaxyStorm.Data.Repositories;
     using Logic.Core;
+    using Logic.Core.Ships;
+    using Logic.Core.Technologies;
 
     public class FleetService : IFleetService
     {
@@ -19,6 +21,129 @@
         {
             this.users = users;
             this.logic = logic;
+        }
+
+        public TimeSpan? ScheduleRecruitScout(string userId, int amount)
+        {
+            var user = this.users
+                  .All()
+                  .Include(x => x.PlayerObject)
+                  .FirstOrDefault(u => u.Id == userId);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            var player = user.PlayerObject;
+
+            if (!this.CanRecruit(player, amount, this.logic.Ships.Scout, this.logic.Technologies.CheaperFleet))
+            {
+                return null;
+            }
+            else
+            {
+                var requiredResources = this.logic.Ships.Scout.RequiredResourcesToBuild;
+
+                this.SubstractResources(player, requiredResources, amount, this.logic.Technologies.CheaperFleet);
+
+                var fasterConstructionLevel = player.Technologies.FasterConstructionLevel;
+                var fasterConstructionModifier =
+                    this.logic.Technologies.FasterConstruction.Modifier[fasterConstructionLevel];
+
+                var timespanToRecruitBeforeTech = TimeSpan.FromTicks((long) (this.logic.Ships.Scout.BuildTime.Ticks*amount * player.Planet.Shard.BuildSpeed));
+
+                var timespan = timespanToRecruitBeforeTech -
+                               TimeSpan.FromTicks((long) (timespanToRecruitBeforeTech.Ticks*fasterConstructionModifier));
+
+                player.Units.CurrentlyRecruiting = CurrentlyRecruiting.Scout;
+                player.Units.StartTime = DateTime.Now;
+                player.Units.EndTime = DateTime.Now.AddTicks(timespan.Ticks);
+                player.Units.AmountRecruiting = amount;
+
+                this.users.Update(user);
+                this.users.SaveChanges();
+
+                return timespan;
+            }
+        }
+
+        public TimeSpan? ScheduleRecruitCarrier(string userId, int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TimeSpan? ScheduleRecruitFighter(string userId, int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TimeSpan? ScheduleRecruitInterceptor(string userId, int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TimeSpan? ScheduleRecruitBomber(string userId, int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TimeSpan? ScheduleRecruitJuggernaut(string userId, int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CompleteRecruiting(string userId)
+        {
+            var user = this.users
+                      .All()
+                      .Include(x => x.PlayerObject)
+                      .FirstOrDefault(u => u.Id == userId);
+
+            if (user == null)
+            {
+                return;
+            }
+
+            var player = user.PlayerObject;
+
+            switch (player.Units.CurrentlyRecruiting)
+            {
+                    case CurrentlyRecruiting.Scout:
+                    player.Units.ScoutsQuantity += player.Units.AmountRecruiting;
+                    player.Points.PointsNeutral += 5 * player.Units.AmountRecruiting;
+                    break;
+                    case CurrentlyRecruiting.Carrier:
+                    player.Units.CarriersQuantity += player.Units.AmountRecruiting;
+                    player.Points.PointsNeutral += 3 * player.Units.AmountRecruiting;
+                    break;
+                    case CurrentlyRecruiting.Fighter:
+                    player.Units.FighterQuantity += player.Units.AmountRecruiting;
+                    player.Points.PointsNeutral += 10 * player.Units.AmountRecruiting;
+                    break;
+                    case CurrentlyRecruiting.Interceptor:
+                    player.Units.InterceptorQuantity += player.Units.AmountRecruiting;
+                    player.Points.PointsNeutral += 12 * player.Units.AmountRecruiting;
+                    break;
+                    case CurrentlyRecruiting.Bomber:
+                    player.Units.BomberQuantity += player.Units.AmountRecruiting;
+                    player.Points.PointsNeutral += 20 * player.Units.AmountRecruiting;
+                    break;
+                    case CurrentlyRecruiting.Juggernaut:
+                    player.Units.JuggernautQuantity += player.Units.AmountRecruiting;
+                    player.Points.PointsNeutral += 50 * player.Units.AmountRecruiting;
+                    break;
+                default:
+                    break;
+            }
+
+            player.Units.CurrentlyRecruiting = CurrentlyRecruiting.None;
+            player.Units.StartTime = null;
+            player.Units.EndTime = null;
+            player.Units.AmountRecruiting = 0;
+
+            this.users.Update(user);
+            this.users.SaveChanges();
         }
 
         public Units GetPlayerFleet(string userId)
@@ -34,6 +159,53 @@
             }
 
             return user.PlayerObject.Units;
+        }
+
+        private bool CanRecruit(PlayerObject pO, int amount, IShip shipLogic, ITechnology techLogic)
+        {
+            if (pO.Units.CurrentlyRecruiting != CurrentlyRecruiting.None)
+            {
+                return false;
+            }
+
+            if (amount <= 0)
+            {
+                return false;
+            }
+
+            if (pO.Buildings.BarracksLevel < shipLogic.Prerequisite)
+            {
+                return false;
+            }
+
+            var requiredResources = shipLogic.RequiredResourcesToBuild;
+            var cheaperFleetModifier = techLogic.Modifier[pO.Technologies.CheaperFleetLevel];
+
+            var requiredEnergy = (requiredResources[0] - (int) (cheaperFleetModifier*requiredResources[0]))*amount;
+            var requiredCrystals = (requiredResources[1] - (int)(cheaperFleetModifier * requiredResources[1])) * amount;
+            var requiredMetal = (requiredResources[2] - (int)(cheaperFleetModifier * requiredResources[2])) * amount;
+
+            return pO.Resources.Energy > requiredEnergy
+                   && pO.Resources.Crystal > requiredCrystals
+                   && pO.Resources.Metal > requiredMetal;
+        }
+
+        private void SubstractResources(PlayerObject pO, int[] resources, int amount, ITechnology techLogic)
+        {
+            if (resources.Length < 3)
+            {
+                throw new ArgumentException();
+            }
+
+            var cheaperFleetModifier = techLogic.Modifier[pO.Technologies.CheaperFleetLevel];
+
+            var requiredEnergy = (resources[0] - (int)(cheaperFleetModifier * resources[0])) * amount;
+            var requiredCrystals = (resources[1] - (int)(cheaperFleetModifier * resources[1])) * amount;
+            var requiredMetal = (resources[2] - (int)(cheaperFleetModifier * resources[2])) * amount;
+
+            pO.Resources.Energy -= requiredEnergy;
+            pO.Resources.Crystal -= requiredCrystals;
+            pO.Resources.Metal -= requiredMetal;
         }
     }
 }
