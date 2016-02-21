c1e24a3b0838d0ec4bd41f0132940174df8f905e
Pip3r4o <pip3r4o@gmail.com>
2016-02-21 10:30:39 +0200

    Added player object mock

---
+++ Source/GalaxyStorm/Tests/GalaxyStorm.Common.Tests/Mocks/MocksFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace GalaxyStorm.Common.Tests.Mocks
+{
+    using Data.Models.PlayerObjects;
+    using Data.Repositories;
+    using Repositories;
+
+    public class MocksFactory
+    {
+        public static IRepository<PlayerObject> PlayerObjectsRepository
+        {
+            get { return PlayerObjectsRepositoryMock.Create(); }
+        }
+    }
+}
