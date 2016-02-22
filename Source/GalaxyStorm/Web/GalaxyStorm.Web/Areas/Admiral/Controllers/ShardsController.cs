c0a05c3ec73324d63254ddc8a870b8d0d23afca5
Pip3r4o <pip3r4o@gmail.com>
2016-02-22 12:16:47 +0200

    Added kendo configuration for grid support Added custom kendo theme

--- Source/GalaxyStorm/Web/GalaxyStorm.Web/Areas/Admiral/Controllers/ShardsController.cs
+++ Source/GalaxyStorm/Web/GalaxyStorm.Web/Areas/Admiral/Controllers/ShardsController.cs
@@ -1,14 +1,66 @@
-﻿namespace GalaxyStorm.Web.Areas.Admiral.Controllers
-{
-    using System.Web.Mvc;
-    using Infrastructure;
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using GalaxyStorm.Data.Models;
 
-    public class ShardsController : AdminController
+namespace GalaxyStorm.Web.Areas.Admiral.Controllers
+{
+    public class ShardsController : Controller
     {
-        // GET: AdmiralsQuarters/Shards
+        private GalaxyStormDbContext db = new GalaxyStormDbContext();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Shards_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            IQueryable<Shard> shards = db.Shards;
+            DataSourceResult result = shards.ToDataSourceResult(request, shard => new {
+                Id = shard.Id,
+                Title = shard.Title,
+                ShardSize = shard.ShardSize,
+                BuildSpeed = shard.BuildSpeed,
+                IsLocked = shard.IsLocked
+            });
+
+            return Json(result);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Shards_Update([DataSourceRequest]DataSourceRequest request, Shard shard)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new Shard
+                {
+                    Id = shard.Id,
+                    Title = shard.Title,
+                    ShardSize = shard.ShardSize,
+                    BuildSpeed = shard.BuildSpeed,
+                    IsLocked = shard.IsLocked
+                };
+
+                db.Shards.Attach(entity);
+                db.Entry(entity).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            return Json(new[] { shard }.ToDataSourceResult(request, ModelState));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
