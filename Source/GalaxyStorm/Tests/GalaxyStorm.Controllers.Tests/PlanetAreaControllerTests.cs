417d4a6abd7e6094757b7dd492b9f915ba59ac66
Pip3r4o <pip3r4o@gmail.com>
2016-02-24 03:42:01 +0200

    Mocked PlayerService Tested PreviewController in Area/Planet

--- Source/GalaxyStorm/Tests/GalaxyStorm.Controllers.Tests/PlanetAreaControllerTests.cs
+++ Source/GalaxyStorm/Tests/GalaxyStorm.Controllers.Tests/PlanetAreaControllerTests.cs
@@ -1,7 +1,10 @@
 ﻿namespace GalaxyStorm.Controllers.Tests
 {
     using System;
+    using AutoMapper;
     using Common.Tests.Mocks;
+    using Data.Models;
+    using Logic.Core;
     using NUnit.Framework;
     using TestStack.FluentMVCTesting;
     using ViewModels.Common;
@@ -16,34 +19,52 @@
         public void PlanetAreaControllerTestsSetup()
         {
             var serviceMock = MocksFactory.PlayerService;
-            var logicMock = MocksFactory.LogicProvider;
+            var logic = new LogicProvider();
 
-            this.previewController = new PreviewController(serviceMock, logicMock);
+            this.previewController = new PreviewController(serviceMock, logic);
+
+            Mapper.CreateMap<Planet, PlanetViewModel>();
         }
 
         [Test]
         public void PreviewIndexShouldWorkCorrectly()
         {
+            const int expectedBarracksLevel = 8;
+            const int expectedCELevel = 13;
+            const int expectedHQLevel = 13;
+            const int expectedMSLevel = 12;
+            const int expectedRCLevel = 4;
+            const int expectedSCLevel = 13;
+
             this.previewController.WithCallTo(x => x.Index())
                 .ShouldRenderDefaultView()
                 .WithModel<CompletePlayerViewModel>(
                     vM =>
                     {
-
+                        Assert.AreEqual(expectedBarracksLevel, vM.Buildings.Barracks.Level);
+                        Assert.AreEqual(expectedCELevel, vM.Buildings.CrystalExtractor.Level);
+                        Assert.AreEqual(expectedHQLevel, vM.Buildings.Headquarters.Level);
+                        Assert.AreEqual(expectedMSLevel, vM.Buildings.MetalScrapper.Level);
+                        Assert.AreEqual(expectedRCLevel, vM.Buildings.ResearchCentre.Level);
+                        Assert.AreEqual(expectedSCLevel, vM.Buildings.SolarCollector.Level);
                     }).AndNoModelErrors();
         }
 
         [Test]
-        public void PreviewResourcesShouldWorkCorrectly()
+        public void PreviewResourcesChildActionShouldWorkCorrectly()
         {
             this.previewController.WithCallToChild(x => x.Resources())
                 .ShouldRenderView("_Resources").WithModel<ResourcesViewModel>(
                     vM =>
                     {
-
+                        Assert.IsTrue(vM.Metal == 1000);
+                        Assert.IsTrue(vM.Energy == 1000);
+                        Assert.IsTrue(vM.Crystal == 1000);
+                        Assert.IsTrue(vM.CrystalPerHour > 100);
+                        Assert.IsTrue(vM.EnergyPerHour > 100);
+                        Assert.IsTrue(vM.MetalPerHour > 100);
                     })
                 .AndNoModelErrors();
-
         }
     }
 }
