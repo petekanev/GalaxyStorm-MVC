fc056890d4fb37f9302104a4f6927b278f29b6e8
Pip3r4o <pip3r4o@gmail.com>
2016-02-24 14:03:08 +0200

    Added error pages and error handling

---
+++ Source/GalaxyStorm/Web/GalaxyStorm.Web/Areas/Public/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿namespace GalaxyStorm.Web.Areas.Public.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ErrorController : Controller
+    {
+        // GET: Public/Error
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+
+        public ActionResult BadRequest()
+        {
+            Response.StatusCode = 400;
+            return View();
+        }
+    }
+}
