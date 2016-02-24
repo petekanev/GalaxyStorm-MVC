1e6d8df08c731be7009df3c4ae2e073a98d592c8
Pip3r4o <pip3r4o@gmail.com>
2016-02-24 02:30:09 +0200

    Added leaderboards page with ajax fetching of information

---
+++ Source/GalaxyStorm/GalaxyStorm.ViewModels/Info/InfoLeaderboardsViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace GalaxyStorm.ViewModels.Info
+{
+    using AutoMapper;
+    using Data.Models.PlayerObjects;
+    using GalaxyStorm.Common.Contracts;
+
+    public class InfoLeaderboardsViewModel : IMapFrom<PlayerObject>, ICustomMapFor
+    {
+        public string DisplayName { get; set; }
+
+        public int PlanetPoints { get; set; }
+
+        public int NeutralPoints { get; set; }
+
+        public int CombatPoints { get; set; }
+
+        public string ShardName { get; set; }
+
+        public long TotalPoints { get { return this.PlanetPoints + this.NeutralPoints + this.CombatPoints; } }
+
+        public void CreateMappings(IConfiguration config)
+        {
+            config.CreateMap<PlayerObject, InfoLeaderboardsViewModel>()
+                .ForMember(i => i.DisplayName, opts => opts.MapFrom(x => x.ApplicationUser.DisplayName))
+                .ForMember(i => i.ShardName, opts => opts.MapFrom(x => x.Planet.Shard.Title))
+                .ForMember(i => i.PlanetPoints, opts => opts.MapFrom(x => x.Points.PointsPlanet))
+                .ForMember(i => i.NeutralPoints, opts => opts.MapFrom(x => x.Points.PointsNeutral))
+                .ForMember(i => i.CombatPoints, opts => opts.MapFrom(x => x.Points.PointsCombat));
+        }
+    }
+}
