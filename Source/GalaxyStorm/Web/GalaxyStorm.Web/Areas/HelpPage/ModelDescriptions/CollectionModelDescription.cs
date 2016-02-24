7fb91501d108850003dd004fd88326e11279c1b5
Pip3r4o <pip3r4o@gmail.com>
2016-02-25 00:46:01 +0200

    configuration update

---
+++ Source/GalaxyStorm/Web/GalaxyStorm.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace GalaxyStorm.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Source/GalaxyStorm/Web/GalaxyStorm.Web/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GalaxyStorm.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
